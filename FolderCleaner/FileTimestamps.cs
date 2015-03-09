d43772661fd8dc832c32c95b4f9c10bdf32606ab
Talles L <talleslasmar@gmail.com>
2015-03-09 16:42:28 -0300

    existing code

---
+++ FolderCleaner/FileTimestamps.cs
@@ -0,0 +1,23 @@
+﻿namespace FolderCleaning
+{
+    /// <summary>
+    /// File available timestamps.
+    /// </summary>
+    public enum FileTimestamps
+    {
+        /// <summary>
+        /// Date of the file creation.
+        /// </summary>
+        Creation,
+
+        /// <summary>
+        /// Date of the last access of the file.
+        /// </summary>
+        LastAccess,
+
+        /// <summary>
+        /// Date of the last write of the file.
+        /// </summary>
+        LastWrite
+    }
+}
